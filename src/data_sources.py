8623b994df831bf0af02d0d0605dc9579d063ad5
Huzefa Raja <ajarhuzefa@gmail.com>
2020-04-28 06:59:05 -0400

    Update models

--- src/data_sources.py
+++ src/data_sources.py
@@ -16,9 +16,35 @@ Base = declarative_base()
 class ReutersDoc(Base):
     __tablename__ = 'reuters_docs'
     id = db.Column(db.Integer, primary_key=True)
-    doc_id = db.Column(db.String(length=900))
-    categories = db.Column(db.String())
-    body = db.Column(db.String())
+    doc_id = db.Column(db.String(length=255), index=True)
+    categories = db.Column(db.String(length=255))
+    body = db.Column(db.TEXT())
+
+
+class ReutersQuote(Base):
+    __tablename__ = 'reuters_quote'
+    id = db.Column(db.Integer, primary_key=True)
+    doc_id = db.Column(db.String(length=255), index=True)
+    _doc_id = db.Column(db.Integer, db.ForeignKey("reuters_docs.id"))
+    start = db.Column(db.Integer)
+    end = db.Column(db.Integer)
+    prefix = db.Column(db.Integer)
+    suffix = db.Column(db.Integer)
+    body = db.Column(db.TEXT())
+    entity_start = db.Column(db.Integer)
+    entity_end = db.Column(db.Integer)
+    entity_id = db.Column(db.Integer, db.ForeignKey("reuters_entity.id"))
+
+
+class ReutersEntity(Base):
+    __tablename__ = 'reuters_entity'
+    id = db.Column(db.Integer, primary_key=True)
+    doc_id = db.Column(db.String(length=255), index=True)
+    _doc_id = db.Column(db.Integer, db.ForeignKey("reuters_docs.id"))
+    start = db.Column(db.Integer)
+    end = db.Column(db.Integer)
+    name = db.Column(db.String(length=255), index=True)
+    type = db.Column(db.String(length=50))
 
 Session = sessionmaker(bind=engine)
 session = Session()
